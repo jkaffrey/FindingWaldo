d87a202224e33f68ddeed418a63018997bffca5a
Jeremy <jkaffrey@gmail.com>
2016-05-11 12:47:29 -0600

    file reorganization

--- findWaldo.js
+++ findWaldo.js
@@ -11,7 +11,26 @@ var findDatWaldo = function(node, funk) {
   }
 };
 
+findDatWaldo(document.head, function(node) {
+
+  if (typeof node.style !== 'undefined' && node.style) {
+
+    if (node.innerHTML.toLowerCase() === 'waldo') {
+
+      node.style.display = 'inline';
+      console.log(node.innerHTML);
+    }
+  }
+});
+
 findDatWaldo(document.body, function(node) {
 
-  alert(node.tagName);
+  if (typeof node.style !== 'undefined' && node.style) {
+
+    if (node.innerHTML.toLowerCase() === 'waldo') {
+
+      node.style.display = 'inline';
+      console.log(node.innerHTML);
+    }
+  }
 });
