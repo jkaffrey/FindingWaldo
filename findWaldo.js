f499aa1a28e2c1aaa230ebd6d39b12933209a57a
Jeremy <jkaffrey@gmail.com>
2016-05-11 13:00:17 -0600

    Removed search of head element

--- findWaldo.js
+++ findWaldo.js
@@ -15,18 +15,6 @@ var findDatWaldo = function(node, funk) {
   }
 };
 
-// findDatWaldo(document.head, function(node) {
-//
-//   if (typeof node.style !== 'undefined' && node.style) {
-//
-//     if (node.innerHTML.toLowerCase() === 'waldo') {
-//
-//       node.style.display = 'inline';
-//       console.log(node.innerHTML);
-//     }
-//   }
-// });
-
 findDatWaldo(document.body, function(node) {
 
   if (typeof node.style !== 'undefined' && node.style) {
